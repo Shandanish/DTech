e8c1a611a5450a59b20c3e2695d435ef7f93eae6
Shandanish <91622503+Shandanish@users.noreply.github.com>
2026-02-26 12:45:00 +0500

    posts.js

--- posts.js
+++ posts.js
@@ -23,5 +23,22 @@ window.postsData = [
     duration: "5:32",
     views: "8.2K"
   }
+  ,
+    {
+    "id": 1772091872094,
+    "type": "software",
+    "title": "Safe Exam Browser Stable Version",
+    "description": "Safe Exam Browser (SEB) is an open-source web browser environment designed to conduct online exams securely by temporarily turning a computer into a locked-down workstation. It restricts access to system functions, other websites, and unauthorized applications to maintain academic integrity",
+    "image": "https://avatars.githubusercontent.com/u/13450095?s=280&v=4",
+    "downloadUrl": "https://www.mediafire.com/file/cakwv0zk4isv3wc/SEB_3.8.0.742_SetupBundle.exe/file",
+    "version": "3.08.01",
+    "fileSize": "300mb",
+    "platform": "Windows",
+    "category": "Education",
+    "date": "2026-02-26",
+    "featured": true,
+    "downloads": 0,
+    "badge": "POPULAR"
+}
 
 ];
