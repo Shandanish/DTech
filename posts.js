ef5e970c97e5198949a28ab4da71e99cce51ef7f
Shandanish <91622503+Shandanish@users.noreply.github.com>
2026-02-26 12:41:12 +0500

    posts.js

--- posts.js
+++ posts.js
@@ -22,18 +22,6 @@ window.postsData = [
     videoUrl: "https://www.youtube.com/watch?v=UgOizAhudsk",
     duration: "5:32",
     views: "8.2K"
-  },
-  {
-     id: 2,
-    type: "software",
-    title: "Safe Exam Browser Stable version",
-    description: "Secure browser for online exams. Prevents cheating by blocking unauthorized websites and apps during tests. Perfect for students and teachers.",
-    image: "https://avatars.githubusercontent.com/u/13450095?s=280&v=4",
-    downloadUrl: "https://www.mediafire.com/file/cakwv0zk4isv3wc/SEB_3.8.0.742_SetupBundle.exe/file",
-    version: "3.10.1",
-    fileSize: "300 MB",
-    platform: "Windows",
-    category: "Education",
-    downloads: "15.4K"
-  },
+  }
+
 ];
